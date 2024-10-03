8c72b39aac1af399a8a8725d28111a2a8a0dcfb7
Shimadakunn <lcombaret@hotmail.co.jp>
2024-10-03 10:26:23 +0200

    chore: Update logo size and alignment in hero section

--- src/sections/hero.tsx
+++ src/sections/hero.tsx
@@ -56,7 +56,7 @@ export default function Hero() {
    </div>
  </div>
 
- <Image src={"/ill.svg"} alt="logo" width={100} height={100} className="w-[70%] " />
+ <Image src={"/ill.svg"} alt="logo" width={100} height={100} className="w-[65%] " />
 
  <div className='w-full text-center'>
    <div className='text-3xl text-[#DBCAF4] mb-2'>
