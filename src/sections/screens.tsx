4d7b37aa42cc8722ff4f2c11de09df702bbb2ce2
Shimadakunn <lcombaret@hotmail.co.jp>
2024-10-04 13:30:02 +0200

    chore: Update screens section styling in home page

--- src/sections/screens.tsx
+++ src/sections/screens.tsx
@@ -3,7 +3,7 @@
   export default function Faq() {
     return (
   
-        <section className=" dark:bg-darkBg bg-bg pt-20 pb-10 font-base lg:pt-[100px] lg:pb-[50px] border-b-4 border-black">
+        <section className=" dark:bg-darkBg bg-bg pt-14 pb-10 font-base lg:pt-[100px] lg:pb-[50px] border-b-4 border-black">
           <h2 className=" text-center text-3xl font-heading md:text-3xl mb-10 lg:text-5xl">
             Application
           </h2>
